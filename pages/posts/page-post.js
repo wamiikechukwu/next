9fe506236f306048b4ca33932d77c2e4c31a4bbf
wami ikechukwu <wamiikechukwu@gmail.com>
2023-08-25 16:26:02 +0100

    working with static files

--- pages/posts/page-post.js
+++ pages/posts/page-post.js
@@ -1,3 +1,21 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Script from 'next/script';
+
+
 export default function FirstPost(){
-    return <h1>First Post</h1>
+    return(
+    <>  
+    <Head>
+      <title>This is the page-post</title>
+    </Head>
+    <Script
+    src="https://connect.facebook.net/en_US/sdk.js"
+    strategy="lazyonload"
+    onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}/>
+    <img src="/../public/images/profile.jpg" alt="Your Name"/>
+      <h1>First Post</h1>
+      <h2><Link href="/">back to home</Link></h2>
+    </>
+    )
 }
