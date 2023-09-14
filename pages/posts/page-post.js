503edec481b8ac853212f3ce731d6da23ea90dc8
wami ikechukwu <wamiikechukwu@gmail.com>
2023-09-14 16:12:07 +0100

    added utility css and others

--- pages/posts/page-post.js
+++ pages/posts/page-post.js
@@ -1,21 +1,30 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import Script from 'next/script';
+import Image from 'next/image'
+import Layout from '../../component/layout';
 
 
-export default function FirstPost(){
-    return(
-    <>  
-    <Head>
-      <title>This is the page-post</title>
-    </Head>
-    <Script
-    src="https://connect.facebook.net/en_US/sdk.js"
-    strategy="lazyonload"
-    onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}/>
-    <img src="/../public/images/profile.jpg" alt="Your Name"/>
-      <h1>First Post</h1>
-      <h2><Link href="/">back to home</Link></h2>
+export default function FirstPost() {
+  return (
+    <>
+      <Layout>
+
+        <Head>
+          <title>This is the page-post</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyonload"
+          onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)} />
+        <Image
+          src="/images/profile.jpg"
+          width={144}
+          height={144}
+          alt='am image' />
+        <h1>First Post</h1>
+        <h2><Link href="/">back to home</Link></h2>
+      </Layout>
     </>
-    )
+  )
 }
